86c563fa1ea2e1fbc40f41f59357f287e366b30a
sandym <91996681+sandyml@users.noreply.github.com>
2022-09-22 14:34:04 -0400

    moved state and effect to maincontainer

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
+import Stock from "./Stock";
+import { stocksUrl } from "../Global";
 
 function MainContainer() {
-  
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch(stocksUrl)
+    .then(resp => resp.json())
+    .then(data => setStocks(data))
+  }, [])
+
+  // everytime we render we iterate always use map
+  const renderStocks = () => {
+    return stocks.map(stock => {
+      return <Stock key={stock.id} stock={stock}/>
+    })
+  }
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer renderStocks={renderStocks} />
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,22 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { stocksUrl } from "../Global";
 import Stock from "./Stock";
 
-function StockContainer() {
-  const [stocks, setStocks] = useState([]);
-
-  useEffect(() => {
-    fetch(stocksUrl)
-    .then(resp => resp.json())
-    .then(data => setStocks(data))
-  }, [])
-
-  // everytime we render we iterate always use map
-  const renderStocks = () => {
-    return stocks.map(stock => {
-      return <Stock key={stock.id} stock={stock}/>
-    })
-  }
+function StockContainer({ renderStocks }) {
 
   return (
     <div>
