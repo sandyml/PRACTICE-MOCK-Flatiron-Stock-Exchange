ec61d01ddf937faf555b29fa69db692cbcc5d7bc
sandym <91996681+sandyml@users.noreply.github.com>
2022-09-22 13:57:40 -0400

    updated fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import MainContainer from "./MainContainer";
 
 function App() {
+  const [stocks, setStocks] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:3001/stocks`) 
+    .then(resp => resp.json())
+    .then(setStocks)
+  }, [])
+
+  
   return (
     <div>
       <Header />
